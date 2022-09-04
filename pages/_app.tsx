e32d9ef5ce028eb4b1f72c7326ec6436ea570a35
sklevzhic <sklevzhic@gmail.com>
2022-09-04 22:32:38 +0300

    added analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,26 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import {Provider} from "react-redux";
 import {store} from "../store";
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Provider store={store}>
+
+    <Script
+        strategy="lazyOnload"
+        src="https://www.googletagmanager.com/gtag/js?id=G-LSYP07Q0S2"
+    />
+
+    <Script id={"ganalytics"} strategy="lazyOnload">
+      {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-LSYP07Q0S2');
+        `}
+    </Script>
+
     <Component {...pageProps} />
   </Provider>
 }
